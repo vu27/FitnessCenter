bca000753fb076cde0a5c1013946262f9ee6cc2f
Tony Vu <52386267+tonyvu1@users.noreply.github.com>
2020-10-26 05:42:47 -0500

    add models

--- gym/models.py
+++ gym/models.py
@@ -1,23 +1,44 @@
 from django.db import models
 
 
-class Member(models.Model):  # Article
+class MembershipTier(models.Model):
+    tier_name = models.CharField(primary_key=True)
+    name = models.CharField(max_length=30)
+    price = models.FloatField(max_length=None)
+
+
+class Member(models.Model):
+    member_id = models.IntegerField(primary_key=True, max_length=None)
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    email = models.EmailField(max_length=100)
-    is_account_active = models.BooleanField(null=True)
-    total_paid = models.FloatField()
-    membership_tier = models.CharField(max_length=30)
-
-    class Meta:
-        db_table = "members"
+    email = models.EmailField(max_length=75)
+    is_account_active = models.BooleanField(default=True)
+    total_paid = models.FloatField(max_length=None)
+    tier_name = models.ForeignKey(MembershipTier, on_delete=models.CASCADE)
 
 
-class MembershipTier(models.Model):  # Reporter
-    tier_code = models.IntegerField(primary_key=True)
-    name = models.CharField(max_length=30)
-    price = models.FloatField()
-    member_id = models.ForeignKey(Member, on_delete=models.CASCADE)
+class Facility(models.Model):
+    facility_name = models.CharField(primary_key=True, max_length=75)
+    tier_name = models.ManyToManyField(MembershipTier)
 
-    class Meta:
-        db_table = "membership_tiers"
+
+class Employee(models.Model):
+    employee_id = models.IntegerField(primary_key=True, max_length=None)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    phone = models.IntegerField(max_length=12)
+    facility = models.ForeignKey(Facility, on_delete=models.CASCADE)
+
+
+class Job(models.Model):
+    job_code = models.IntegerField(primary_key=True, max_length=25)
+    description = models.CharField(max_length=500)
+    salary = models.FloatField(max_length=None)
+    employee_id = models.ForeignKey(Employee, on_delete=models.CASCADE)
+
+
+class Equipment(models.Model):
+    equip_name = models.IntegerField(primary_key=True, max_length=75)
+    price = models.FloatField(max_length=None)
+    quantity = models.IntegerField(max_length=500)
+    facility_name = models.ForeignKey(Facility, on_delete=models.CASCADE)
