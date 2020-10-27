40ad75c7f6636d51849f0a214831747298af7d48
Tony Vu <52386267+tonyvu1@users.noreply.github.com>
2020-10-27 05:31:09 -0500

    update data models

--- gym/models.py
+++ gym/models.py
@@ -1,62 +1,77 @@
 from django.db import models
 
+MAX_LENGTH = 75
+
 
 class MembershipTier(models.Model):
-    tier_name = models.CharField(primary_key=True, max_length=30)
-    name = models.CharField(max_length=30)
-    price = models.FloatField(max_length=None)
+    tier_code = models.AutoField(primary_key=True)
+    tier_name = models.CharField(max_length=MAX_LENGTH)
+    tier_price = models.FloatField(max_length=None)
 
     class Meta:
         db_table = "membership_tier"
 
 
 class Member(models.Model):
-    member_id = models.IntegerField(primary_key=True)
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    email = models.EmailField(max_length=75)
-    is_account_active = models.BooleanField(default=True)
-    total_paid = models.FloatField(max_length=None)
-    tier_name = models.ForeignKey(MembershipTier, on_delete=models.CASCADE)
+    mem_id = models.AutoField(primary_key=True)
+    mem_fname = models.CharField(max_length=MAX_LENGTH)
+    mem_lname = models.CharField(max_length=MAX_LENGTH)
+    mem_email = models.EmailField(max_length=MAX_LENGTH)
+    mem_is_active = models.BooleanField(default=True)
+    mem_total_paid = models.FloatField(max_length=None)
+    tier_code = models.ForeignKey(
+        MembershipTier, db_column="tier_code", on_delete=models.CASCADE
+    )
 
     class Meta:
         db_table = "member"
 
 
 class Facility(models.Model):
-    facility_name = models.CharField(primary_key=True, max_length=75)
-    tier_name = models.ManyToManyField(MembershipTier)
+    fac_id = models.AutoField(primary_key=True)
+    fac_name = models.CharField(max_length=MAX_LENGTH)
 
     class Meta:
         db_table = "facility"
 
 
+class SharedFacility(models.Model):
+    id = models.AutoField(primary_key=True)
+    fac_id = models.ForeignKey(Facility, db_column="fac_id", on_delete=models.CASCADE)
+    tier_code = models.ForeignKey(
+        MembershipTier, db_column="tier_code", on_delete=models.CASCADE
+    )
+
+    class Meta:
+        db_table = "shared_facility"
+
+
 class Employee(models.Model):
-    employee_id = models.IntegerField(primary_key=True)
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    phone = models.IntegerField()
-    facility = models.ForeignKey(Facility, on_delete=models.CASCADE)
+    emp_id = models.AutoField(primary_key=True)
+    emp_fname = models.CharField(max_length=MAX_LENGTH)
+    emp_lname = models.CharField(max_length=MAX_LENGTH)
+    emp_phone = models.IntegerField()
+    fac_id = models.ForeignKey(Facility, db_column="fac_id", on_delete=models.CASCADE)
 
     class Meta:
         db_table = "employee"
 
 
 class Job(models.Model):
-    job_code = models.IntegerField(primary_key=True)
-    description = models.CharField(max_length=255)
-    salary = models.FloatField(max_length=None)
-    employee_id = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    job_code = models.AutoField(primary_key=True)
+    job_dec = models.CharField(max_length=255)
+    job_salary = models.FloatField(max_length=None)
+    emp_id = models.ForeignKey(Employee, db_column="emp_id", on_delete=models.CASCADE)
 
     class Meta:
         db_table = "job"
 
 
 class Equipment(models.Model):
-    equip_name = models.IntegerField(primary_key=True)
-    price = models.FloatField(max_length=None)
-    quantity = models.IntegerField()
-    facility_name = models.ForeignKey(Facility, on_delete=models.CASCADE)
+    equip_id = models.AutoField(primary_key=True)
+    equip_name = models.CharField(max_length=MAX_LENGTH)
+    equip_quantity = models.IntegerField()
+    fac_id = models.ForeignKey(Facility, db_column="fac_id", on_delete=models.CASCADE)
 
     class Meta:
         db_table = "equipment"
